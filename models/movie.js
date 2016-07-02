4dcfe25c200a5ba7b11da51637c3593145c9b334
Matthew Broatch <mbroatch@gmail.com>
2016-07-01 20:34:20 -0700

    runtime added

--- models/movie.js
+++ models/movie.js
@@ -5,13 +5,14 @@ let request = require('request');
 let movieSchema = new mongoose.Schema({
 	title: {type: String, required:true},
 	year: Number,
-	netflix: String,
+	netflix: Number,
 	imdb: String,
 	poster: String,
 	imdbRating: String,
 	metascore: String,
 	plot: String,
-	actors: String
+	actors: String,
+	runtime: Number
 });
 
 
@@ -25,8 +26,11 @@ movieSchema.methods.getInfoAndSave = function(cb){
 			this.metascore = body.Metascore;
 			this.plot = body.Plot;
 			this.actors = body.Actors;
+			this.runtime = parseInt(body.Runtime.slice(0,-4));
+			console.log('this.runtime',this.runtime);
 
 			// TODO: remove api key
+			
 			request.get("https://api.themoviedb.org/3/find/"+ this.imdb + "?external_source=imdb_id&api_key=d30a3154577a074b866f6a5123696362", (error,response,body) => {
 			body = JSON.parse(body);
 				this.poster = "https://image.tmdb.org/t/p/original" + body.movie_results[0].poster_path;
