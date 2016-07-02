8d1e8699c331d9e0949da5273bb59b5de6aec9a0
ymoregola <ymoregola@gmail.com>
2016-07-01 20:53:19 -0700

    created basic home and match page

--- public/js/module.js
+++ public/js/module.js
@@ -8,6 +8,12 @@ app.config(function($stateProvider, $urlRouterProvider){
 		url: '/', 
 		templateUrl: 'html/home.html', 
 		controller: 'homeCtrl'
+	})   	
+
+	.state('match', {
+		url: '/match', 
+		templateUrl: 'html/match.html', 
+		controller: 'matchCtrl'
 	})    
 
 	.state('admin', {
