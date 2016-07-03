65f656bf2b40bd0ed0a679f42ec1580b196cc624
Matthew Broatch <mbroatch@gmail.com>
2016-07-02 20:36:24 -0700

    state transitions

--- public/js/module.js
+++ public/js/module.js
@@ -1,18 +1,17 @@
 "use strict;"
 
-var app = angular.module('appName', ['ui.bootstrap','ui.router','xeditable', 'ngAnimate']); 
+var app = angular.module('appName', ['ui.bootstrap','ui.router','xeditable', 'ngAnimate', 'anim-in-out']); 
 app.config(function($stateProvider, $urlRouterProvider){
 
 
-    $stateProvider
-      .state('admin', {url: '/admin', templateUrl: 'html/admin.html', controller: 'adminController'})
+	$stateProvider
+	.state('admin', {url: '/admin', templateUrl: 'html/admin.html', controller: 'adminController'})
 
 
 	.state('home', {
 		url: '/', 
 		templateUrl: 'html/home.html', 
 		controller: 'homeCtrl',
-		// params: {output: null}
 	})   	
 
 	.state('match', {
@@ -22,13 +21,7 @@ app.config(function($stateProvider, $urlRouterProvider){
 		params: {output: null}
 	})    
 
-	// .state('admin', {
-	// 	url: '/admin', 
-	// 	templateUrl: 'html/admin.html', 
-	// 	controller: 'adminCtrl'
-	// })   
-
-	$urlRouterProvider.otherwise('/');
+		$urlRouterProvider.otherwise('/');
 
 });
 
